e0abccc44efbade7851160feb9abd48cc1a13800
Farhad Davaripour, Ph.D <65896745+Farhad-Davaripour@users.noreply.github.com>
2022-06-16 08:47:58 -0600

    Update Homepage.py

--- my_app/Homepage.py
+++ my_app/Homepage.py
@@ -27,15 +27,15 @@ with col2:
     st.write('\n')
     st.image("https://github.com/Farhad-Davaripour/CFRP_Reinforced_HDD_overbend/blob/main/scroll%20down%20for%20more.png?raw=true",width=120)
 # Inpus on the sidebar
-st.sidebar.title("Overview")
+st.sidebar.title("Input parameters:")
 #
-with st.sidebar.expander("Pipeline inputs"):
-    diameter_over_thickness = slide_bar = st.slider("Pipe's Diameter over wall-thickness", value=40, 
+with st.sidebar.expander("Pipeline inputs:"):
+    diameter_over_thickness = slide_bar = st.slider("Pipe's Diameter over wall-thickness:", value=40, 
                         min_value=20, max_value=50)
-    pressure = slide_bar = st.slider('Internal pressure (MPa)', value=9, 
+    pressure = slide_bar = st.slider('Internal pressure (MPa):', value=9, 
                       min_value=2, max_value=10) 
 with st.sidebar.expander("CFRP inputs"):
-    CFRP_thickness = slide_bar = st.slider('Thickness of the CFRP wrap (mm)', value=5, 
+    CFRP_thickness = slide_bar = st.slider('Thickness of the CFRP wrap (mm):', value=5, 
                         min_value=0, max_value=6)
     FO = slide_bar = st.radio("Fibre orientation",('Circumferencial', 'Longitudinal', 'Multi-directional'))
     if FO=='Circumferencial':
