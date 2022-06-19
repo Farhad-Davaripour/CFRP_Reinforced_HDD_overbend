994fcfdfa10fdf06fe158df2c108e59bad7aa327
Farhad Davaripour <farhad.davaripour@gmail.com>
2022-06-19 10:44:41 -0600

    Add: adding the app.py file for the Heroku deployment

---
+++ my_app/app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.markdown("""<b><h4 style='text-align:left;'>Overview:</h4></b>""", unsafe_allow_html=True)
+st.write('The following video provides a brief overview on how to use the dashbaord:')
+st.video("Introduction.mp4")
