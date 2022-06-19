8d6242a094ac7d2bca958d8db9822447f7e1c6ac
Farhad Davaripour <farhad.davaripour@gmail.com>
2022-06-19 13:17:03 -0600

    Fix: changint the mp4 location

--- my_app/Homepage.py
+++ my_app/Homepage.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.markdown("""<b><h4 style='text-align:left;'>Overview:</h4></b>""", unsafe_allow_html=True)
 st.write("""The following video provides a brief overview on how to use the dashbaord. Note that the video does not run on Mac
 or IOS OS at this time.""")
-with open('https://drive.google.com/file/d/1Wz0S8aJBeTT9ofpHSSQKm9XEmnsbLP9r/view?usp=sharing', 'rb') as video:
+with open('introeuction.mp4', 'rb') as video:
     st.video(video)
