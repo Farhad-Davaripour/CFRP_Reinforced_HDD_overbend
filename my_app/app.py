a3060b4f4790088e8587bdaa1c3e6a7dca8f2af5
Farhad Davaripour <farhad.davaripour@gmail.com>
2022-06-19 10:48:26 -0600

    Fix: Deleting the app.pyu

--- my_app/app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.markdown("""<b><h4 style='text-align:left;'>Overview:</h4></b>""", unsafe_allow_html=True)
-st.write('The following video provides a brief overview on how to use the dashbaord:')
-st.video("Introduction.mp4")
