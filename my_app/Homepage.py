902181947d15c4ff2aa422f7a2ef3a1e2607b41a
Farhad Davaripour <farhad.davaripour@gmail.com>
2022-06-19 12:43:59 -0600

    Fix: fixing the video on ios

--- my_app/Homepage.py
+++ my_app/Homepage.py
@@ -2,4 +2,6 @@ import streamlit as st
 st.markdown("""<b><h4 style='text-align:left;'>Overview:</h4></b>""", unsafe_allow_html=True)
 st.write("""The following video provides a brief overview on how to use the dashbaord. Note that the video does not run on Mac
 or IOS OS at this time.""")
-st.video("Introduction.mp4")
+video_file = open('Introduction.mp4', 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
