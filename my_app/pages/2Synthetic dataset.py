390d7986c66e8c76e47d0d9d358fdb8af8dc990b
Farhad Davaripour <farhad.davaripour@gmail.com>
2022-08-01 08:13:40 -0600

    Add: adding the databse to the synthetic dataset page

--- my_app/pages/2Synthetic dataset.py
+++ my_app/pages/2Synthetic dataset.py
@@ -2,21 +2,13 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import pandas as pd
 st.markdown("""The synthetic dataset is generated from multiple finite element analyses. Please contact 
-Farhad Davaripour on his [Linked-in profile]("https://www.linkedin.com/in/farhad-davaripour/") if you wish to review the dataset.""")
-###
-# names = ['Farhad Davaripour']
-# usernames = ['fd7385']
-# passwords = ['4562193IRANsaze']
-# hashed_passwords = stauth.Hasher(passwords).generate()
-# authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
-#     'some_cookie_name','some_signature_key')
-# name, authentication_status, username = authenticator.login('Login','main')
-# if authentication_status:
-#     authenticator.logout('Logout', 'main')
-#     st.write('Welcome *%s*' % (name))
-#     st.write('I should find a way to show the data in here.')
-#     # pd.read_excel()
-# elif authentication_status == False:
-#     st.error('Username/password is incorrect')
-# elif authentication_status == None:
-#     st.warning('Please enter your username and password')
+Farhad Davaripour on his [Linked-in profile]("https://www.linkedin.com/in/farhad-davaripour/") for more infor about the dataset.""")
+# data acquisition
+st.write('The following table lists the analyses conducted using the validated FE model:')
+##
+df = pd.read_excel('data/data.xlsx', 'Combined')
+features_list = ['t_steel', 'D/t', 'L_CFRP', 't_CFRP', 'fibre_orient', 'pressure']
+target_variable = ['No_CFRP_vm','peak_vm', 'peak_vm_CFRP', 'vm_reduction_perc']
+df = df.dropna()
+## displaying the data
+st.table(df[features_list + target_variable].reset_index())
