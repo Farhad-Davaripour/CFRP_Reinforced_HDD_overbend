f0aee5794b542e5fcb6dd956851b2e0fad7c2a91
Farhad Davaripour <farhad.davaripour@gmail.com>
2022-08-01 11:13:30 -0600

    fix: fixing a typo in the synthetic database page

--- my_app/pages/2Synthetic dataset.py
+++ my_app/pages/2Synthetic dataset.py
@@ -3,7 +3,7 @@ import streamlit_authenticator as stauth
 import pandas as pd
 from pathlib import Path
 st.markdown("""The synthetic dataset is generated from multiple finite element analyses. Please contact 
-Farhad Davaripour on his [Linked-in profile]("https://www.linkedin.com/in/farhad-davaripour/") for more infor about the dataset.""")
+Farhad Davaripour on his [Linked-in profile]("https://www.linkedin.com/in/farhad-davaripour/") for more info about the dataset.""")
 # data acquisition
 st.write('The following table lists the analyses conducted using the validated FE model:')
 ##
