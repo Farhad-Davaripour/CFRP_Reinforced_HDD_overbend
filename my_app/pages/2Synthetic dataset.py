b82d22a6246141dfffe5c4bcbd95f60a876e1b45
Farhad Davaripour <farhad.davaripour@gmail.com>
2022-08-01 18:31:50 -0600

    add: adding definition for fibre orietation in the synthetic dataset page

--- my_app/pages/2Synthetic dataset.py
+++ my_app/pages/2Synthetic dataset.py
@@ -6,7 +6,9 @@ st.subheader('Synthetic dataset')
 st.markdown("""The synthetic dataset is generated from multiple finite element analyses. Please contact 
 Farhad Davaripour on his [Linked-in profile]("https://www.linkedin.com/in/farhad-davaripour/") for more info about the dataset.""")
 # data acquisition
-st.write('The following table lists the analyses conducted using the validated FE model:')
+st.write("""The following table lists the analyses conducted using the validated FE model. Note that the fibre orientation of
+0, 1, and 2 corresponds to the circumferential direction, longitudinal direction, and the combination of the two cases 
+before (multi-direction).""")
 ##
 df = pd.read_excel("data/data.xlsx")
 features_list = ['t_steel', 'D/t', 'L_CFRP', 't_CFRP', 'fibre_orient', 'pressure']
